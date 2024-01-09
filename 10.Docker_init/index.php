e98aaf9dc63b6f2c968c825b1488b2d6aebff969
Carlos Alonso-Martinez <carlos.martinez@mataro.epiaedu.cat>
2024-01-09 07:32:04 +0100

    Update exemples

---
+++ 10.Docker_init/index.php
@@ -0,0 +1,23 @@
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script>
+ $(document).ready(function(){
+     setInterval(ajaxcall, 1000);
+ });
+ function ajaxcall(){
+     $.ajax({
+         url: 'gettime.php',
+         success: function(data) {
+             data = data.split(':');
+             $('#hours').html(data[0]);
+             $('#minutes').html(data[1]);
+             $('#seconds').html(data[2]);
+         }
+     });
+ }
+</script>
+
+<?php
+echo "<h1>Tu mensaje aquí</h1>";
+echo 
+'<span id="hours">0</span>:<span id="minutes">0</span>:<span id="seconds">0</span>';
+?>
